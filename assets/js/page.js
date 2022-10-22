96279a6c8c3263a1d4ad28a42b222d091cac56c9
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-10-23 01:07:47 +0200

    \update\

--- assets/js/page.js
+++ assets/js/page.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     // we create the warning that lets the user know that this program has the option to generate a plugboard configuration
     // or he can chose to create is ow plugboard configuration
+    plugBoard = document.getElementById("plugBoard")
     plugwarn = document.createElement("span");
     plugwarn.setAttribute('id', "warningSpan")
     plugBoard.appendChild(plugwarn);
@@ -15,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         plugBoardSpan.setAttribute('id', "spanLetter-" + letterArray[i])
         plugBoardSpan.innerHTML = letterArray[i]
 
-        var plugInput = document.createElement("input");
+        plugInput = document.createElement("input");
         plugInput.setAttribute("type", "text");
         plugInput.setAttribute('id', "letter-" + i,)
 
@@ -28,10 +29,22 @@ document.addEventListener("DOMContentLoaded", function () {
     plugBoard.appendChild(sendPlugBoardConf);
 
     for (i = 0, j = 3; i != j;i++ ){
-        rotors = document.createElement("input")
-        rotors.setAttribute('type', "number");
-        rotors.setAttribute('id', "rotor-"+i);
+        rotors = document.getElementById("rotors")
+        
+        rotor = document.createElement("input")
+        rotor.setAttribute('type', "number");
+        rotor.setAttribute('id', "rotor-"+i);
+
+
+        rotors.appendChild(rotor)
     }
 });
 // TODO add this  part to the documentation 
 // creating html elements with javascript 
+// first create a variable like in the exsample in the ("") you select the tipe of item you want to create
+// rotors = document.createElement("input")
+// then yu set the the atribute ("type","something") 
+// you also can add a id 
+// rotors.setAttribute('id', "rotor-"+i);
+// and to finish you ned to add the element to the variable you create earlier
+
