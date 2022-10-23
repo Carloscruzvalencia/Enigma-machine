9ecedd4e226bd13deb88b018b27853b69bbbb0ad
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-10-23 06:55:09 +0200

    add comments to page.js

--- assets/js/page.js
+++ assets/js/page.js
@@ -4,45 +4,55 @@ document.addEventListener("DOMContentLoaded", function () {
     // we create the warning that lets the user know that this program has the option to generate a plugboard configuration
     // or he can chose to create is ow plugboard configuration
     plugBoard = document.getElementById("plugBoard")
-    plugwarn = document.createElement("span");
-    plugwarn.setAttribute('id', "warningSpan")
-    plugwarn.innerHTML = "if the letters are not changed, new letters will be chosen randomly"
-    plugBoard.appendChild(plugwarn);
+    plugWarning = document.createElement("span");
+    plugWarning.setAttribute('id', "warningSpan")
+    plugWarning.innerHTML = "if the letters are not changed, new letters will be chosen randomly"
+    plugBoard.appendChild(plugWarning);
 
+    // this variable controls when the content has to jump to the next block
     org = 0
-    for (k = 0 ;k!= 3 ;k++){
-        plugdiv = document.createElement("div")
-        plugdiv.setAttribute("id","separator"+k)
-        plugBoard.appendChild(plugdiv)
-        
-        for (i = 0, j = 9; i != j; i++) {
-            plugdivorg = document.createElement("div")
-            plugdivorg.setAttribute("id","content"+org)
-            plugdiv.appendChild(plugdivorg)
+    // there i will create 4 things: 2 div / 1 span / 1 input
+    // the first div is used for making groups of 3 div inside i create groups of 9 div (also know as second div)
+    // the 2 div is used to contain: 1 span and 1 input
 
+    for (i = 0 ;i!= 3 ;i++){
+        // first the creation of the first div  (its inside a loop so will create 3 div)
+        plugDiv = document.createElement("div")
+        plugDiv.setAttribute("id","separator"+i)
+        plugBoard.appendChild(plugDiv)
+        
+        for (j = 0, k = 9; j != k; j++) {
+            // then i create the 9 groups of div 
+            plugDivOrg = document.createElement("div")
+            plugDivOrg.setAttribute("id","content"+org)
+            plugDiv.appendChild(plugDivOrg)
+
+            // here i create the span tha only says what letter is
             plugBoardSpan = document.createElement("span");
             plugBoardSpan.setAttribute('id', "spanLetter-" + letterArray[org])
             plugBoardSpan.innerHTML = letterArray[org]
             
+            // here i create the input that will be used to make the first phase of encryption
             plugInput = document.createElement("input");
             plugInput.setAttribute("type", "text");
             plugInput.setAttribute('id', "letter-" + org, )
 
+            // this is what says when the content has to jump to the next block
             if (org <= 9){
-                plugorg = document.getElementById("content"+org)
-                plugorg.appendChild(plugBoardSpan);
-                plugorg.appendChild(plugInput);
+                plugOrg = document.getElementById("content"+org)
+                plugOrg.appendChild(plugBoardSpan);
+                plugOrg.appendChild(plugInput);
             }
             else if(org >= 10 && org <= 20){
-                plugorg = document.getElementById("content"+org)
-                plugorg.appendChild(plugBoardSpan);
-                plugorg.appendChild(plugInput);
+                plugOrg = document.getElementById("content"+org)
+                plugOrg.appendChild(plugBoardSpan);
+                plugOrg.appendChild(plugInput);
             }
 
             else if (org >= 20 && org <=27){
-                plugorg = document.getElementById("content"+org)
-                plugorg.appendChild(plugBoardSpan);
-                plugorg.appendChild(plugInput);
+                plugOrg = document.getElementById("content"+org)
+                plugOrg.appendChild(plugBoardSpan);
+                plugOrg.appendChild(plugInput);
             }
             org++
         }
@@ -54,9 +64,9 @@ document.addEventListener("DOMContentLoaded", function () {
     sendPlugBoardConf.setAttribute('id', "plugData");
     plugBoard.appendChild(sendPlugBoardConf);
 
-    rotorwarn = document.createElement("span")
-    rotorwarn.innerHTML = "put a number betwen 0 and 27 in each rotor or let the program decide"
-    rotors.appendChild(rotorwarn)
+    rotorWarning = document.createElement("span")
+    rotorWarning.innerHTML = "put a number between 0 and 27 in each rotor or let the program decide"
+    rotors.appendChild(rotorWarning)
 
     for (i = 0, j = 3; i != j; i++) {
         rotors = document.getElementById("rotors")
@@ -70,9 +80,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 // TODO add this  part to the documentation 
 // creating html elements with javascript 
-// first create a variable like in the exsample in the ("") you select the tipe of item you want to create
+// first create a variable like in the example in the ("") you select the type of item you want to create
 // rotors = document.createElement("input")
-// then yu set the the atribute ("type","something") 
+// then yu set the the attribute ("type","something") 
 // you also can add a id 
 // rotors.setAttribute('id', "rotor-"+i);
 // and to finish you ned to add the element to the variable you create earlier
