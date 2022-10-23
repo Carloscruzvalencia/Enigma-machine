033f06ec6f145a4c90909d4600c8b9f685b9122e
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-10-23 16:58:41 +0200

    update page.js \n adding the documentation to each proses in the code

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // si
     letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
     // we create the warning that lets the user know that this program has the option to generate a plugboard configuration
     // or he can chose to create is ow plugboard configuration
     plugBoard = document.getElementById("plugBoard")
@@ -11,10 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // this variable controls when the content has to jump to the next block
     org = 0
+
+    // this section is only made for eliminate repetitive code made by me in the html and also helps wen the styles are madded
     // there i will create 4 things: 2 div / 1 span / 1 input
     // the first div is used for making groups of 3 div inside i create groups of 9 div (also know as second div)
     // the 2 div is used to contain: 1 span and 1 input
-
     for (i = 0 ;i!= 3 ;i++){
         // first the creation of the first div  (its inside a loop so will create 3 div)
         plugDiv = document.createElement("div")
@@ -39,17 +41,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // this is what says when the content has to jump to the next block
             if (org <= 9){
+                // the first group and his 9 elements
                 plugOrg = document.getElementById("content"+org)
                 plugOrg.appendChild(plugBoardSpan);
                 plugOrg.appendChild(plugInput);
             }
             else if(org >= 10 && org <= 20){
+                // the second group and his 9 elements
                 plugOrg = document.getElementById("content"+org)
                 plugOrg.appendChild(plugBoardSpan);
                 plugOrg.appendChild(plugInput);
             }
 
             else if (org >= 20 && org <=27){
+                // the third group and his 9 elements
                 plugOrg = document.getElementById("content"+org)
                 plugOrg.appendChild(plugBoardSpan);
                 plugOrg.appendChild(plugInput);
@@ -58,16 +63,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-
+    // this button is used to get the user input and send it to the plugboard  
     sendPlugBoardConf = document.createElement("input");
     sendPlugBoardConf.setAttribute('type', "button");
     sendPlugBoardConf.setAttribute('id', "plugData");
     plugBoard.appendChild(sendPlugBoardConf);
 
+    // here i warn the user to make an input of numbers 
     rotorWarning = document.createElement("span")
     rotorWarning.innerHTML = "put a number between 0 and 27 in each rotor or let the program decide"
     rotors.appendChild(rotorWarning)
 
+    // here i create the rotors
     for (i = 0, j = 3; i != j; i++) {
         rotors = document.getElementById("rotors")
 
@@ -78,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function () {
         rotors.appendChild(rotor)
     }
 });
-// TODO add this  part to the documentation 
+
 // creating html elements with javascript 
 // first create a variable like in the example in the ("") you select the type of item you want to create
 // rotors = document.createElement("input")
